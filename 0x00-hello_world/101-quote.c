96aade13a187aea4e167b456ea7a64840fa86734
smartfusion01 <itsealonge@yahoo.com>
2022-04-14 09:45:41 -0700

    Program to print a given quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,9 @@
 /**
   *main - Entry point
   *Return: 1
-  *Function that prints a given quote
   */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n". stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
