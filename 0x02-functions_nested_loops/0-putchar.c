da61a6cce15286048844ceffd9afb9cc7845c483
smartfusion01 <itsealonge@yahoo.com>
2022-04-15 13:48:14 -0700

    Program that prints out putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,18 @@
 #include "main.h"
 
 /**
-  * main - Check description
-  * Description: It prints the word _putchar, followed by a new line.
+  * main - Program that prints out _putchar, followed by a new line
   * Return: 0.
   * Betty coding style
   */
 int main(void)
 {
 	char word[8] = "_putchar";
-	int a;
+	int i;
 
-	for (a = 0; a < 8; a++)
-		_putchar(word[a]);
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
 	_putchar('\n');
 
 	return (0);
-}
+}i
