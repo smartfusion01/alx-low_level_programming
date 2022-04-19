5c8387b2d1196dbc8cf979a40cefcf6034f94bb1
smartfusion01 <itsealonge@yahoo.com>
2022-04-19 08:31:45 -0700

    A function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+  * times_table - Check description
+  * Description: It prints 9 times table starting with 0
+  * Return: Nothing.
+  * Betty coding style
+  */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			if ((c / 10) == 0)
+			{
+				if (b != 0)
+					_putchar(' ');
+				_putchar(c + '0');
+
+				if (b == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
